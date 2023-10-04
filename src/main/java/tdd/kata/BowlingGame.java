726eb458bd03f77e98a99e07fe0913aafa452cc1
AudranL <91188510+AudranL@users.noreply.github.com>
2023-10-04 13:27:58 +0200

    add feature for spare

--- src/main/java/tdd/kata/BowlingGame.java
+++ src/main/java/tdd/kata/BowlingGame.java
@@ -1,10 +1,38 @@
 package tdd.kata;
 
-public class BowlingGame {
-	public void roll(int i) {
-	}
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-	public int score() {
-		return 9;
-	}
+public class BowlingGame {
+    private final List<Integer> rolls = new ArrayList<>();
+
+    public void roll(int i) {
+        this.rolls.add(i);
+    }
+
+    public int score() {
+        int score = 0;
+        for (int index = 0; index < rolls.size(); index++) {
+            if (index + 1 < rolls.size() && frameScore(index) == 10) {
+                score += frameScore(index) + firstRollOfNextFrame(index);
+                index++;
+            } else {
+                score += currentRollScore(index);
+            }
+        }
+        return score;
+    }
+
+    private Integer currentRollScore(int index) {
+        return rolls.get(index);
+    }
+
+    private Integer firstRollOfNextFrame(int index) {
+        return rolls.get(index + 2);
+    }
+
+    private int frameScore(int index) {
+        return currentRollScore(index) + rolls.get(index + 1);
+    }
 }
--- src/test/java/tdd/kata/BowlingGameTest.java
+++ src/test/java/tdd/kata/BowlingGameTest.java
@@ -5,22 +5,43 @@ import org.junit.jupiter.api.Test;
 import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 class BowlingGameTest {
+    @Test
+    void score_return_the_number_of_pin_when_a_uniq_success_roll_is_done_with_9pins() {
+        BowlingGame game = new BowlingGame();
 
+        game.roll(9);
+        fulfillGameWithGutterRoll(game, 19);
 
-	@Test
-	void score_return_the_number_of_pin_when_a_uniq_success_roll_is_done_with_9pins() {
-		BowlingGame game = new BowlingGame();
+        assertThat(game.score()).isEqualTo(9);
+    }
 
-		game.roll(9);
-		fullfilGameWithGlutterRoll(game);
+    @Test
+    void score_return_the_number_of_pin_when_a_uniq_success_roll_is_done_with_8pins() {
+        BowlingGame game = new BowlingGame();
 
-		assertThat(game.score()).isEqualTo(9);
-	}
+        game.roll(8);
+        fulfillGameWithGutterRoll(game, 19);
 
-	private static void fullfilGameWithGlutterRoll(BowlingGame game) {
-		IntStream.range(0, 19).forEach(
-				(idx) -> game.roll(0)
-		);
-	}
+        assertThat(game.score()).isEqualTo(8);
+    }
+
+    @Test
+    void score_return_the_number_of_pin_when_a_spare_is_done() {
+        BowlingGame game = new BowlingGame();
+
+        game.roll(8);
+        game.roll(2);
+        game.roll(1);
+        fulfillGameWithGutterRoll(game, 17);
+
+        assertThat(game.score()).isEqualTo(12);
+    }
+
+    private static void fulfillGameWithGutterRoll(BowlingGame game, int complementaryRolls) {
+        IntStream.range(0, complementaryRolls).forEach(
+                (idx) -> game.roll(0)
+        );
+    }
 }
